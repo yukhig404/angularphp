4595bef818c8bc2a36db3fed14a824a7a9255ae7
yukhig <yukhig@gmail.com>
2015-01-26 15:23:06 +0900

    api catch params

--- controller.js
+++ controller.js
@@ -1,12 +1,43 @@
 var app = angular.module('app', ['ngResource']);
 
 app.controller('MainCtrl', function($scope, $resource) {
-  var Student = $resource('students.php', {}, {
-    'update': {method: 'PUT'}
+  var Student = $resource('students.php', {page:'@page', size:'@size', sort:'@sort'}, {
+    'update': {method: 'PUT'},
+    'query2': {method: 'GET'},
+    'save': {
+      method: 'POST',
+      params : {
+        pass : '@pass',
+      },
+  },
   });
-  $scope.students = Student.query();
-  $scope.add = function() {
-    $scope.students.push(Student.save());
+  $scope.students = Student.query({
+              // page : $scope.page,
+              // size : $scope.size,
+              // sort : $scope.key + ',' + $scope.order,
+            });
+ $scope.add = function() {
+
+    var postData = {
+      pass : 'hgoehogehgoe'
+    };
+
+    $scope.students.push(Student.save(
+      {},
+      postData,
+      function(data) {console.log(data);}));
+  };
+
+   $scope.add = function() {
+
+    var postData = {
+      pass : 'hgoehogehgoe'
+    };
+
+    $scope.students.push(Student.save(
+      {},
+      postData,
+      function(data) {console.log(data);}));
   };
 });
 
--- students.php
+++ students.php
@@ -3,16 +3,32 @@
 $pdo = new PDO('mysql:dbname=school', 'root');
 switch ($_SERVER['REQUEST_METHOD']) {
 case 'GET':
-  // query
-  $st = $pdo->query("SELECT * FROM students");
-  echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
-  break;
+  if(!isset($_GET['page'])){
+    // query
+    $st = $pdo->query("SELECT * FROM students");
+    echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
+    break;
+  }else if($_GET['page'] == 5){ //?page=5のとき
+    // query2
+    $st = $pdo->query("SELECT * FROM students");
+    echo('hoge');
+    break;
+  }else{
+    echo('error');
+    break;
+  }
 case 'POST':
   // save
-  $pdo->query("INSERT INTO students VALUES()");
-  $st = $pdo->query('SELECT LAST_INSERT_ID() AS id FROM students');
-  echo json_encode($st->fetch(PDO::FETCH_ASSOC));
-  break;
+  $in = json_decode(file_get_contents('php://input'), true);
+  if(!isset($in['pass'])){
+    // echo('error');
+    break;
+  }else if($in['pass'] == 'hgoehogehgoe'){  //passが正しいとき
+    $pdo->query("INSERT INTO students VALUES()");
+    $st = $pdo->query('SELECT LAST_INSERT_ID() AS id FROM students');
+    echo json_encode($st->fetch(PDO::FETCH_ASSOC));
+    break;
+  }
 case 'PUT':
   // update
   $in = json_decode(file_get_contents('php://input'), true);
